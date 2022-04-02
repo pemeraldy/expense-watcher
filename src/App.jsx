a26121ddbc43316f609719e01e06923b4064549f
Peter Olu <pemeraldy@gmail.com>
2022-04-02 16:14:36 +0100

    installed hero icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from "react";
 import "./App.css";
+import SideBar from "./components/SideBar";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="flex  h-screen">
-      <div className=" h-full  md:w-[278px] bg-black"></div>
-      <div className=" h-full md:w-[402px] bg-gray-300"></div>
+      <div className=" h-full  md:w-[20%] bg-black">
+        <SideBar />
+      </div>
+      <div className=" h-full md:w-[30%] bg-gray-300"></div>
       <div className=" flex-1 h-full bg-gray-600"></div>
     </div>
   );
---
+++ src/components/SideBar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function SideBar() {
+  return (
+    <div className='w-full md:p-11'>
+        <h1 className='text-white'> Hello</h1>
+    </div>
+  )
+}
