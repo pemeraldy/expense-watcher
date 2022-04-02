28c39055bc0c5c928f2d9b14a9b76a79188dbe6b
Peter Olu <pemeraldy@gmail.com>
2022-04-02 18:21:19 +0100

    attempt at the first card

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import { useState } from "react";
 import "./App.css";
+import CardList from "./components/CardList";
 import SideBar from "./components/SideBar";
 
 
 function App() {  
   return (
     <div className="flex  h-screen">
-      <div className=" h-full  md:w-[20%] bg-black">
+      <div className=" hidden md:block h-full md:w-[20%] bg-black">
         <SideBar />
       </div>
-      <div className=" h-full md:w-[25%] bg-gray-50 border-r border-gray-300"></div>
+      <div className=" md:p-11 h-full md:w-[28%] bg-gray-50 sm:border-r-2 border-gray-200">
+        <h1 className="font-bold text-2xl mb-[50px]">Card List</h1>
+        <CardList />
+      </div>
       <div className=" flex-1 h-full bg-gray-50"></div>
     </div>
   );
---
+++ src/components/CardList.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function CardList() {
+  return (
+    <div className="w-full flex justify-center">
+      <div className="relative flex flex-col justify-center items-center">
+        <div className="md:w-[252px] absolute md:h-[150px] rounded-lg bg-[#F5F0E8]"></div>
+
+        <div className="md:w-[319px] relative -bottom-5 md:h-[170px] rounded-lg bg-[#EAF3FB]"></div>
+      </div>
+    </div>
+  );
+}
